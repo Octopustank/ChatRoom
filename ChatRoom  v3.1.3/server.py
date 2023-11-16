cac8347d340863a00daf649d7132227e772cee9e
kwicxy <2871029476@qq.com>
2023-11-16 12:39:43 +0800

    Meet out new friend -- A Brand New Index Page!

--- ChatRoom  v3.1.3/server.py
+++ ChatRoom  v3.1.3/server.py
@@ -1,133 +1,45 @@
+'''ChatRoom主程序'''
+
 from flask import Flask, render_template, request, redirect, send_file, flash
 import datetime as dt
 import json as js
 import os
 from urllib.parse import quote
 import sys
-#################### 判断Pyhon版本来判断使用场景 ####################
+from init import *
+
 IP = "0.0.0.0"
-PORT = 80
+PORT = 114
 
-#################### 工具函数和初始化 ####################
-# 获取工作路径
-def init_path():
-    global PATH
-    path = os.path.abspath(__file__)
-    PATH = os.path.dirname(path)
-    print(f"[INIT] PATH: {PATH}")
-
-# 当天日志文件和用户文件初始化
-def init_file():
-    print("[INIT] READ DATA_FILE&USERS")
-    global DATA_LST, LOGFILE, USER_LST, USERFILE, floor
-    DATA_LST = [];
-    init_path()
-    DATE_STR = dt.datetime.now().strftime("%y-%m-%d")
-    logfiles = os.path.join(PATH,"logfiles")
-    LOGFILE = os.path.join(logfiles,f"{DATE_STR}.json")
-    bin_ = os.path.join(PATH,"bin")
-    USERFILE = os.path.join(bin_,"users.json")
-    USER_LST = read_file(USERFILE)
-    # 如果当天已存在日志文件，则接着盖楼，否则新建
-    if os.path.isfile(LOGFILE):
-        DATA_LST = read_file(LOGFILE)
-        floor = DATA_LST[-1][1]
-        print(f"[INIT] 读取日志:{LOGFILE}")
-    else: 
-        floor = 1
-        print("[INIT] 无已存在日志")
-    init_backlog()
-
-# 前7天（最多）历史记录初始化
-def init_backlog():
-    global BACK_LOG_LST, BACK_LOG_LEN
-    print("[INIT] GRAB LOG")
-    latest_date = dt.datetime.now()
-    former_date = latest_date - dt.timedelta(days=1)
-    former_date_str = former_date.strftime("%y-%m-%d")
-    logfiles = os.path.join(PATH,"logfiles")
-    LOGFILE_p = os.path.join(logfiles,f"{former_date_str}.json")
-    BACK_LOG_LST = []
-    cnt = 1
-    # 循环读日志，条件为日志文件存在且为七天内
-    while cnt <=7:
-        if os.path.isfile(LOGFILE_p):
-            TEMP_LST = read_file(LOGFILE_p)
-            TEMP_LST.append(former_date_str)
-            BACK_LOG_LST.append(TEMP_LST)
-        former_date = former_date - dt.timedelta(days=1)
-        former_date_str = former_date.strftime("%y-%m-%d")
-        logfiles = os.path.join(PATH,"logfiles")
-        LOGFILE_p = os.path.join(logfiles,f"{former_date_str}.json")
-        cnt += 1
-    BACK_LOG_LEN = len(BACK_LOG_LST)
-    print(f"[INIT] {BACK_LOG_LEN} LOGS IN TOTAL")
-
-#初始化文件传输
-def init_file_sending():
-    global PATH_FILE_JS, PATH_FILES, FILE_LIST
-    print("[INIT] READ FILE_LIST")
-    path = os.path.join(PATH, "files_sending")
-    PATH_FILES = os.path.join(path, "files")
-    PATH_FILE_JS = os.path.join(path, "file_data.json")
-
-    FILE_LIST = []
-    if os.path.exists(PATH_FILE_JS):
-        FILE_LIST = read_file(PATH_FILE_JS)#读上传的文件列表[[filename,user,time]]
-        file_list = os.listdir(PATH_FILES)
-        p = 0; l = len(FILE_LIST); flag = False#检查文件是否都存在:
-        while p < l:
-            if not FILE_LIST[p][0] in file_list:
-                drop_one = FILE_LIST.pop(p)
-                print(f"[FILE_LIST] REMOVE: {drop_one}")
-                flag = True
-            else: p += 1
-            l = len(FILE_LIST)
-        l = len(file_list)#检查是否有文件夹中存在但不在列表中的文件:
-        for p in range(l):
-            files_in_list = list(map(lambda x:x[0],FILE_LIST))#把文件日志中文件名(index=0)取出
-            if not file_list[p] in files_in_list:
-                FILE_LIST.append([file_list[p],"-/- -:-:-","-.-.-","server"])
-                print(f"[FILE_LIST] ADD: {file_list[p]}")
-                flag = True
-        if flag: write_file(PATH_FILE_JS, FILE_LIST)#若有文件变动，修正该问题
-
-#处理文件名防止重名
-def check_filename(path,original_name):
-    exist_names = os.listdir(path)
-    first_name, last_name = os.path.splitext(original_name)
-    name = [first_name, last_name, 0]#文件名各部分和序号(若为0则实际不加)
-    make_name = lambda x:x[0]+(f"({str(x[2])})"if x[2]!=0 else "")+x[1]#拼接文件名函数
-    while True:
-        if not make_name(name) in exist_names:#该文件名独一无二
-            break
-        name[2]+=1#该文件名已存在，序号增加
-    name = make_name(name)
-    return os.path.join(path,name), name
-
-# 通用读文件管道
-def read_file(F):
-    with open(F, 'r', encoding='utf-8') as f:
-        data = f.read().encode(encoding='utf-8')
-        result = js.loads(data)
-    return result
-
-# 通用写文件管道
-def write_file(F, data):
-    with open(F,"w",encoding="utf-8") as f:
-        js.dump(data, f, ensure_ascii=False, indent=True)
-
-#################### Flask主程序 ####################
-app = Flask("chat_room")
+app = Flask("chat_room", static_url_path="/static", static_folder="./templates/static")
 app.config['JSON_AS_ASCII'] = False
 app.secret_key = "hard"
+
+# 主页
+@app.route("/")
+def index():
+    addr = request.remote_addr
+    if not addr in USER_LST:            # 用户未注册，重定向至注册页面
+        return redirect('/register')
+    else:
+        userid = USER_LST[addr]
+        HOUR_NOW = dt.datetime.now().hour
+        if HOUR_NOW in range(5,12):
+            return render_template("index.html", greetings="Good Morning", userid=userid)
+        elif HOUR_NOW in range(12,18):
+            return render_template("index.html", greetings="Good Afternoon", userid=userid)
+        elif HOUR_NOW in range(18,22):
+            return render_template("index.html", greetings="Good Evening", userid=userid)
+        else:
+            return render_template("index.html", greetings="Good Night", userid=userid)
+
 
 # 聊天主页面
 @app.route("/chatroom/")          
 def main():
     addr = request.remote_addr
     if not addr in USER_LST:            # 用户未注册，重定向至注册页面
-        return redirect('/chatroom/register')
+        return redirect('/register')
     else:
         userid = USER_LST[addr]
         if len(DATA_LST) == 0:
@@ -159,9 +71,10 @@ def send():
         return render_template('send.html', python_hint="发送成功！",last=name)
 
 # 新用户注册
-@app.route("/chatroom/register", methods=["GET","POST"])
+@app.route("/register", methods=["GET","POST"])
 def register():
     addr = request.remote_addr
+    origin = request.args.get("from")
     if request.method == 'GET':
         if addr in USER_LST:
             return redirect('/chatroom/')
@@ -239,8 +152,11 @@ def refresh():
         init_file_sending()
         return f"刷新文件列表成功<p>{'<p>'.join(list(map(str,FILE_LIST)))}"
     return redirect('/chatroom/')
+
 ########################################
 if __name__ =="__main__":
-    init_file()
-    init_file_sending()
+    PATH = init_path()
+    DATA_LST, LOGFILE, USER_LST, USERFILE, floor = init_file()
+    BACK_LOG_LST, BACK_LOG_LEN = init_backlog()
+    PATH_FILE_JS, PATH_FILES, FILE_LIST = init_file_sending()
     app.run(host=IP, port=PORT, debug=True)
